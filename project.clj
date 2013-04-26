1882021cc043d63319c47b1469cdb0a89c4221f8
Martin Trojer <martin.trojer@gmail.com>
2013-04-26 08:00:31 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pipejine "0.1.0"
+(defproject pipejine "0.1.1-SNAPSHOT"
   :description "Flexible, Safe and Simple Multi-Consumer/Producer Clojure Pipelines"
   :url "https://github.com/martintrojer/pipejine"
   :license {:name "Eclipse Public License"
